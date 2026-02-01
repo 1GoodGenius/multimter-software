db04ed24b3a52d865edff716a138fd24c41ff4d6
CI Builder <ci@localhost>
2026-02-02 00:06:15 +0100

    Build: add ecdsa.h shims for include resolution

---
+++ arduino_firmware/ecdsa.h
@@ -0,0 +1,2 @@
+// Shim to expose project include ecdsa.h to Arduino build
+#include "include/ecdsa.h"
---
+++ arduino_firmware/src/ecdsa.h
@@ -0,0 +1,2 @@
+// Shim so files in src/ that include "ecdsa.h" pick up the project header
+#include "../include/ecdsa.h"
